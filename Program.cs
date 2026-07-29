0a10d4d1e6aa612ae8cf83624d8a9bbb7d906396
FelixF <felixfyx@gmail.com>
2026-07-30 07:47:46 +0800

    Clean slate

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DLManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
